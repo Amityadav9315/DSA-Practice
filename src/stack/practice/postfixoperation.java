7d3f83d84ae2215abe3b4dfff40b546cfc6383d1
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-22 11:16:36 +0530

    grsjbbs

--- src/stack/practice/postfixoperation.java
+++ src/stack/practice/postfixoperation.java
@@ -4,6 +4,8 @@ import stack.Stack;
 
 import java.util.Scanner;
 
+import static jdk.vm.ci.code.Location.stack;
+
 public class postfixoperation {
 
     public static int  evalkuateExpression(String expression ){
@@ -12,8 +14,33 @@ public class postfixoperation {
         //get character
         for(int i=0; i<expression.length(); i++){
              char ch=expression.charAt(i);
+              //if it is digit
+             if(Character.isDigit(ch)){
+                 //ch=0
+                 stack.push(ch-'0');
 
-             if(char)
+
+             }
+             else{
+                 int operand1= stack.pop();
+                 int operand2= stack.pop();
+
+                 switch (ch){
+
+                     case '+':
+                         stack.push(operand2+operand1);
+                         break;
+                     case '-':
+                         stack.push(operand2-operand1);
+                         break;
+                     case '*':
+                         stack.push(operand2*operand1);
+                         break;
+                     case '/':
+                         stack.push(operand2/operand1);
+                         break;
+                 }
+             }
 
         }
 

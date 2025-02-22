db874fcaae9d2a2ac923d10fca88148c6df5ebaa
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-22 10:45:32 +0530

    sfbkjgr

--- src/stack/practice/postfixoperation.java
+++ src/stack/practice/postfixoperation.java
@@ -1,26 +1,60 @@
 package stack.practice;
 
-import stack.Stack;
-
-import java.util.Scanner;
+import java.util.Stack;
 
 public class postfixoperation {
 
-    static void postfixOperation(String ch){
-        Stack stack=new Stack(ch.length());
-        for(int i=0 ; i<ch.length(); i++){
-        char str=ch.charAt(i);
 
-        if(str=='1'||str=='2'||str=='3'||str=='4'||str=='5'||str=='6'||str=='6'||str=='7'||str=='8'||str=='9')
+    public static int evaluatePostfix(String expression) {
+
+        Stack<Integer> stack = new Stack<>();
 
 
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+            // If the character is a digit, push it onto the stack
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0'); // Convert character to integer
+            }
+            // If the character is an operator
+            else {
+                // Pop the two operands from the stack
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+
+                // Perform the operation based on the operator
+                int result = 0;
+                switch (ch) {
+                    case '+':
+                        result = operand1 + operand2;
+                        break;
+                    case '-':
+                        result = operand1 - operand2;
+                        break;
+                    case '*':
+                        result = operand1 * operand2;
+                        break;
+                    case '/':
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        System.out.println("Invalid operator: " + ch);
+                        return -1;
+                }
+
+                // Push the result back onto the stack
+                stack.push(result);
+            }
+        }
+
+        // The result is the only item left in the stack
+        return stack.pop();
     }
 
-
     public static void main(String[] args) {
-        Scanner  sc=new Scanner(System.in);
-        String ch=sc.next();
-
-
+        String expression = "23*5+";  // Example postfix expression
+        System.out.println("Postfix Expression: " + expression);
+        System.out.println("Evaluation Result: " + evaluatePostfix(expression));
     }
 }

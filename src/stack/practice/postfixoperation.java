7e2b3439fe3be54426ec07d99ed14e18c1fbf0a6
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-22 10:36:56 +0530

    czdb,dv

--- src/stack/practice/postfixoperation.java
+++ src/stack/practice/postfixoperation.java
@@ -8,6 +8,10 @@ public class postfixoperation {
 
     static void postfixOperation(String ch){
         Stack stack=new Stack(ch.length());
+        for(int i=0 ; i<ch.length(); i++){
+        char str=ch.charAt(i);
+
+        if(str=='1'||str=='2'||str=='3'||str=='4'||str=='5'||str=='6'||str=='6'||str=='7'||str=='8'||str=='9')
 
 
     }

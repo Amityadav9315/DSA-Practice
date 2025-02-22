feedc887d5b5d74105195def0cd9fbc849ce70b3
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-22 10:34:34 +0530

    first commit

---
+++ src/stack/Stack.java
@@ -0,0 +1,69 @@
+package stack;
+
+public class Stack {
+
+    private int[] array;
+    private int maxSize;
+    private int top=-1;
+
+    public Stack(int size){
+        this.maxSize=size;
+        array=new int[maxSize];
+        top=-1;
+
+    }
+    //push operation
+    public void push(int data){
+        if(top==maxSize-1){
+            System.out.println("Stack overflow");
+            return;
+        }
+        else{
+            top++;
+            array[top]=data;
+        }
+    }
+    //check empty
+    public boolean isEmpty(){
+        return top==-1;
+
+    }
+
+    //pop operation
+    public int  pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        else{
+            int popElement=array[top];
+            top--;
+            return popElement;
+        }
+
+    }
+    //peak operation
+    public  int peek(){
+        if(isEmpty()){
+            System.out.println("stack is empty");
+            return -1;
+        }
+        else{
+            return array[top];
+
+        }
+    }
+
+    //display data
+    public void displayElements(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+        }
+        for(int i=0; i<=top; i++){
+            System.out.print(array[i]+"\t");
+        }
+        System.out.println();
+    }
+
+
+}

dbff0458793983da86d08d1ace43d80d25c3bfab
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-22 11:03:32 +0530

    gdsbndgksdg

--- src/stack/practice/BalanceParenthesis.java
+++ src/stack/practice/BalanceParenthesis.java
@@ -11,9 +11,31 @@ public class BalanceParenthesis {
     public static boolean isExpressionBalanced(String expression){
 
         Stack stack=new Stack(expression.length());
+        for(int i=0; i<expression.length(); i++){
+            char ch=expression.charAt(i);
+            if(ch=='{'||ch=='('||ch=='['){
+                stack.push(ch);
+            } else if (ch=='}'||ch==')'||ch==']') {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top= (char) stack.pop();
+                if(!isMatchingPair(top,ch)){
+                    return false;
+
+                }
 
 
-        return false;
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+
 
     }
       private static  boolean isMatchingPair(char open,char close){

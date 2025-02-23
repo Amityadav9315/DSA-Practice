ce6eff679075210dbad914cdbb16b4abd25ff72a
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-23 14:31:29 +0530

    wfdbjfs

--- src/stack/practice/QueueUsingStack.java
+++ src/stack/practice/QueueUsingStack.java
@@ -15,10 +15,11 @@ public class QueueUsingStack {
 
     public QueueUsingStack(int size){
         this.size=size;
-        stack1=new Stack(this.size);
-        stack2=new Stack(this.size);
+        stack1=new Stack();
+        stack2=new Stack();
     }
     public void enqueue(int data){
+
         this.stack1.push(data);
     }
 
@@ -34,11 +35,21 @@ public class QueueUsingStack {
             if (stack2.isEmpty()) {
                 while (!stack1.isEmpty()) {
                     stack2.push(stack1.pop());
+
+
                 }
             }
-            return stack2.pop();
+            return (int) stack2.pop();
         }
+
         
 
+    }
+
+    public int peek(){
+        if(stack1.isEmpty()&&stack2.isEmpty()){
+            System.out.println("Queue is empty");
+            return -1;
+        }
     }
 }

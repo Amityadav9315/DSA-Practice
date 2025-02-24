fdc226a54ba1a8625f807b0e06c6b5480b6d4b03
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-24 10:42:53 +0530

    dbjn.dse

--- src/Recursion/OneToTen.java
+++ src/Recursion/OneToTen.java
@@ -1,8 +1,17 @@
 package Recursion;
 
 public class OneToTen {
+    static void onetoten(int n){
+        System.out.println(n);
+        if(n<11){
+            onetoten(n+1);
+        }
+    }
 
     public static void main(String[] args) {
+
+        System.out.println("Program started");
+        onetoten(1);
 
     }
 }

07abfd3924fa4f0f2f33c63b1c3e0d5d739e2f8b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-24 17:12:32 +0530

    fdkngsd

--- src/Recursion/SumofArray.java
+++ src/Recursion/SumofArray.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 
 public class SumofArray {
 
-    static int sumofArray(int[] arr,int n){
+    static int sumofArray(int[] arr, int n) {
 
 
-        if(n<=0){
+        if (n <= 0) {
             return 0;
         }
-
-        }
+        return arr[n - 1] + sumofArray(arr, n - 1);
 
     }
-
-
 
 
     public static void main(String[] args) {
@@ -25,10 +22,14 @@ public class SumofArray {
         int[] arr=new int[n];
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
-
-
         }
+
+
+
+        int result=sumofArray(arr,n);
+        System.out.println(result);
 
 
     }
 }
+

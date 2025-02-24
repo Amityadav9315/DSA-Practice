dfea4cd963c83ace30b70f118842b843c9734615
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-24 17:01:50 +0530

    fdkngsd

---
+++ src/Recursion/SumofArray.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumofArray {
+
+    static int sumofArray(int[] arr,int n){
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+
+
+        }
+
+
+    }
+}

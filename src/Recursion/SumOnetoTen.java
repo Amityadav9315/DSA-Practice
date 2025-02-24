23bfb1752528d7a4bfab8e7c0290b28dd17b20b8
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-24 17:07:36 +0530

    fdlgs

--- src/Recursion/SumOnetoTen.java
+++ src/Recursion/SumOnetoTen.java
@@ -1,4 +1,21 @@
 package Recursion;
 
 public class SumOnetoTen {
+
+
+    static void sumOnetoTen(int n){
+        int sum=0;
+
+        if(n<10){
+            sum=sum+n;
+            sumOnetoTen(n-1);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        sumOnetoTen(10);
+    }
 }

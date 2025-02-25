d4fc5533c4760e2007dec8318b252b5c5ab33619
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-26 01:05:37 +0530

    sfkhsrsr

--- src/Recursion/TOH/TowerofHionahi.java
+++ src/Recursion/TOH/TowerofHionahi.java
@@ -4,6 +4,9 @@ public class TowerofHionahi {
 
     static  void TOH(int n,char source,char helper,char destination){
 
+        if(n==1){
+            System.out.println("Move disk 1 from"+source+"to"+destination);
+        }
 
     }
 

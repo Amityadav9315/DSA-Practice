88ff317f4929b6704d2366fe69d13e26b083314c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-25 09:51:33 +0530

    wfkfeq

--- src/Recursion/ncr/PascalTriangle.java
+++ src/Recursion/ncr/PascalTriangle.java
@@ -2,11 +2,25 @@ package Recursion.ncr;
 
 public class PascalTriangle {
 
+    static int fact(int n){
+        if(n==1||n==0){
+            return 1;
+        }
+
+        return n*fact(n-1);
+
+    }
+
     static int PascalTriangle(int n,int r){
 
         if(r>0){
             return 0;
         }
+        int factN=fact(n);
+        int factR=fact(r);
+        int factNR=fact(n-r);
+
+        return factN/(factR*(factNR));
 
     }
 

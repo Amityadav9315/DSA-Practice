d093fed10e9299dd11f7dc5931472de28337f6e7
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-25 09:06:26 +0530

    dfskbgrs

--- src/Recursion/ncr/NCRITR.java
+++ src/Recursion/ncr/NCRITR.java
@@ -1,4 +1,19 @@
 package Recursion.ncr;
 
 public class NCRITR {
+
+    static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+
+    //combination
+    //ncr
+    static int Ncr
+
+    public static void main(String[] args) {
+
+    }
 }

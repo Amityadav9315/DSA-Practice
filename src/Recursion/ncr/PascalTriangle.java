b1f8ccc9fb4356d86146dbf6c71b5ddfdfcac763
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-25 09:46:46 +0530

    fwflkw

---
+++ src/Recursion/ncr/PascalTriangle.java
@@ -0,0 +1,16 @@
+package Recursion.ncr;
+
+public class PascalTriangle {
+
+    static int PascalTriangle(int n,int r){
+
+        if(r>0){
+            return 0;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

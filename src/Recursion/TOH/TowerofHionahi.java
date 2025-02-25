94d2982437ced193405f052e80b6bc76b5b64e62
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-26 00:48:32 +0530

    fdshgsr

--- src/Recursion/TOH/TowerofHionahi.java
+++ src/Recursion/TOH/TowerofHionahi.java
@@ -5,12 +5,12 @@ public class TowerofHionahi {
     static  void TOH(int n,char source,char helper,char destination){
 
         if(n==1){
-            System.out.println("Move disk 1 from"+source+"to"+destination);
+            System.out.println("Move disk 1 from" +source+ "to"+destination);
             return;
         }
         //toc logic
         TOH(n-1,source,destination,helper);
-        System.out.println("Move disk"+n+"from"+source+"to"+destination);
+        System.out.println("Move disk"+n+"from" +source+ "to"+destination);
         TOH(n-1,helper,source,destination);
 
     }

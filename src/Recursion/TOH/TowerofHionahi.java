1caaad41cfeefcd45673721d2209cabdd18bf853
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-26 00:42:34 +0530

    fdshgsr

--- src/Recursion/TOH/TowerofHionahi.java
+++ src/Recursion/TOH/TowerofHionahi.java
@@ -6,12 +6,23 @@ public class TowerofHionahi {
 
         if(n==1){
             System.out.println("Move disk 1 from"+source+"to"+destination);
+            return;
         }
+        //toc logic
+        TOH(n-1,source,destination,helper);
+        System.out.println("Move disk"+n+"from"+source+"to"+destination);
+        TOH(n-1,helper,source,destination);
 
     }
 
 
     public static void main(String[] args) {
+
+
+        int n=3;
+        TOH(n,'A','B','C');
+
+
 
     }
 }

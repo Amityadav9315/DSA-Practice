dfc80665b99b85b6357c9f95142b472aa1457c0b
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-26 22:42:53 +0530

    feqklngw

--- src/TreeCodeBinary/Node.java
+++ src/TreeCodeBinary/Node.java
@@ -1,5 +1,15 @@
 package TreeCodeBinary;
 
 public class Node {
+      private int data;
+      private   Node left,right;
+
+      public Node(int data){
+          this .data=data;
+          this
+      }
+
+
+
 
 }
